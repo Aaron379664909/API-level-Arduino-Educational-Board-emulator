ba007ad22c14fc65f8203865ba636173f251ff0c
Aaron379664909 <42068981+Aaron379664909@users.noreply.github.com>
2018-09-24 21:22:52 +0800

    Add files via upload
    for user write their own code to emulate Arduino Educational Board

---
+++ usercode.h
@@ -0,0 +1,23 @@
+#ifndef USERCODE_H
+#define USERCODE_H
+#include <QObject>
+
+class UserCode : public QObject
+{
+    Q_OBJECT
+public:
+    UserCode();
+    void setup();
+    void loop();
+signals:
+    void delay(int ms);
+    void pinMode(int i,int mode);
+    void digitalWrite(int i, bool value);
+    bool digitalRead(int i);
+    int  analogRead(int i);
+    void analogWrite(int i, int value);
+public:
+    void testcode();
+    void testcode1();
+};
+#endif // USERCODE_H
